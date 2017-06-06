c57154d1ff5241080c2aedc82a1dcd8d0607c6b7
berkibal <berkib007@gmail.com>
2017-06-06 12:42:31 +0200

    rendrakas

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # nyilvantarto_flask
-school assignment, no guarantees :)
+school assignment, no guarantees :D under development
+
+# Telepites:
+Python3 kornyezet:
+````
+sudo apt install python3; sudo apt install python3-pip
+````
+Virtualenv kornyezet:
+````
+sudo pip install virtualenv
+````
+nodejs, bower telepítése:
+
+````
+sudo apt-get remove --purge nodejs
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt update;
+sudo apt install nodejs
+npm install --global bower
+````
+
+
+# Hasznalat
+Clone-ozzuk ezt a repot:
+````
+git clone https://github.com/KrupMark/nyilvantarto_flask.git
+````
+Keszitsunk virtualenv-et:
+````
+virtualenv -p python3 ~/virtualenv/nyilvantarto_flask
+````
+Aktivaljuk a virtualenv-et:
+````
+source ~/virtualenv/nyilvantarto_flask/bin/activate
+````
+Telepitsuk a python csomagokat:
+````
+pip install -r requirements.txt
+````
+A clone-ozott nyilvantarto_flask mappan belul telepitsuk a bower csomagokat:
+````
+bower install
+````
+
+# Futtatas
+A nyilvantarto_flask mappan belul futtassuk az appot:
+````
+python app.py
+````
--- app.py
+++ app.py
@@ -1,13 +1,47 @@
 ﻿from flask import Flask
 from flask import render_template
+from flask import request, redirect, url_for
+
 app = Flask(__name__)
 app.debug = True
 
 # http://flask.pocoo.org
+# az index fuggveny rendereli le az index.html file-t
+# a / oldalon get és post method-okat is engedünk
+# https://www.w3schools.com/tags/ref_httpmethods.asp
+# EZ NEM EGY IGAZI BEJELENTKEZO FElULET, igy nem szabad csinalni rendes bejelentkezest
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    # egy error valtozot deklaralunk aminek a kezdo erteken None (azaz semmi)
+    error = None
+    # ha ez a page ugy van meghivva hogy a kliens POST-ot kuld
+    if request.method == 'POST':
+        # a login.html-ben levo form adatokat, kiszedjuk a POST-bol es betoltjuk ket darab valtozoba
+        email = request.form.get('email')
+        passwd = request.form.get('password')
+        # print(request.form.get)
+        print("A megadott emailcim: {}".format(email))
+        print("A megadott jelszo: {}".format(passwd))
+        # a bekuldott adatok kozul ellenorizzuk a felahasznalonevet, es ha az helyes
+        if email == "KrupMark":
+            # atiranyitjuk a felhasznalot a belso oldalra
+            return redirect(url_for('nyilvantarto'))
+        else:
+            # ha rossz adatokat adott meg, akkor egy hibauzenetet kuldonk...
+            error = 'Invalid Credentials. Please try again.'
+    # a flask minden template file-t a templates mappan belul keres.
+    # ha nem POST a http keres, hanem GET, akkor siman csak a login.html-t rendereljuk:
+    return render_template('login.html', error=error)
 
-@app.route("/")
-def hello():
-	return render_template('index.html')
+@app.route("/nyilvantarto")
+def nyilvantarto():
+    user = "berkibalazs"
+    # a user valtozo,az az index.html file-ban is user valtozokent lesz elerheto
+    return render_template('index.html', user=user)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
+
+# react
+# angular js
+# bower.io, npm
